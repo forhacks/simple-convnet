ab90ac6ee8a5592ee544155d70ea09cdc42c0945
thegithubgeek <wfrankw9@gmail.com>
2017-11-02 21:43:54 -0700

    Add im2col

--- cnn_model.py
+++ cnn_model.py
@@ -1,5 +1,26 @@
 import numpy as np
 
+
+def im2col(A, B, skip):
+    # Parameters
+    D, M, N = A.shape
+    col_extent = N - B[1] + 1
+    row_extent = M - B[0] + 1
+
+    # Get Starting block indices
+    start_idx = np.arange(B[0])[:, None] * N + np.arange(B[1])
+
+    # Generate Depth indeces
+    didx = M * N * np.arange(D)
+    start_idx = (didx[:, None] + start_idx.ravel()).reshape((-1, B[0], B[1]))
+
+    # Get offsetted indices across the height and width of input array
+    offset_idx = np.arange(row_extent)[:, None] * N + np.arange(col_extent)
+
+    # Get all actual indices & index into input array for final output
+    out = np.take(A, start_idx.ravel()[:, None] + offset_idx[::skip[0], ::skip[1]].ravel())
+
+
 # def convolve():
 #
 A = np.array([[[1, 1, 2],
